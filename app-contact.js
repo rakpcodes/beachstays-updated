43ea9e0647578117402bdb18e995662321a6707f
rakpcodes <rakpcodes@gmail.com>
2023-02-15 03:33:17 -0500

    updated phone and phone-small media queries

--- app-contact.js
+++ app-contact.js
@@ -33,8 +33,8 @@ const navCallback = (navEntry, navObserver) => {
 }
 
 navOptions = {
-    rootMargin: '-60px',
-    threshold: 0.9,
+    rootMargin: '-45px',
+    threshold: 0.8,
 };
 
 const navObserver = new IntersectionObserver(navCallback, navOptions)
